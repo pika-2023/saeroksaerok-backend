a484268e4824a4cde97d8c3e0b8d07787514f384
hgene0929@gmail.com <hgene0929@gmail.com>
2023-05-18 23:01:22 +0900

    [feat] first commit

---
+++ src/main/java/com/chorokchorok/saeroksaerok/SaeroksaerokApplication.java
@@ -0,0 +1,13 @@
+package com.chorokchorok.saeroksaerok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaeroksaerokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaeroksaerokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chorokchorok/saeroksaerok/SaeroksaerokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chorokchorok.saeroksaerok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaeroksaerokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
